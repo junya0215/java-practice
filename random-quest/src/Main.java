37823293873ff2d7150c4c12f8143161b4f50324
junya0215 <junya285@gmail.com>
2014-03-28 15:38:19 +0900

    Add random-quest

---
+++ random-quest/src/Main.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Main{
+	public static final Random rand = new Random();
+	public static void main(String[] args){
+
+
+		Scanner scan = new Scanner(System.in);
+
+		int command;
+
+		Status pSt = new Status("player",rand.nextInt(99)+1);
+		Status eSt = new Status("enemy",rand.nextInt(99)+1);
+
+
+		System.out.println("敵が現れた!!");
+
+		while(true){
+			System.out.printf("%s:%d %s:%d\n",pSt.getName(),pSt.getHp(), eSt.getName(),eSt.getHp());
+			System.out.println("playerのターン");
+			System.out.println("攻撃(0)/回復(1)/逃げる(2)");
+
+			command = scan.nextInt();
+			if(command == 0){
+				pSt.attack(eSt);
+			}else if(command == 1){
+				pSt.heal();
+			}else if(command == 2){
+				pSt.runAway();
+				break;
+			}
+
+			if(eSt.getHp() <= 0){
+				System.out.println("俺の勝ち!");
+				break;
+			}
+
+			line(35);
+
+			System.out.println("敵のターン");
+			command = rand.nextInt(2);
+			if(command == 0){
+				eSt.attack(pSt);
+			}else if(command == 1){
+				eSt.heal();
+			}
+
+			if(pSt.getHp() <= 0){
+				System.out.println("俺の負け!");
+				break;
+			}
+
+			line(35);
+		}
+	}
+
+
+	public static void line(int l){
+		for(int i = 0; i < l; i++){
+			System.out.print("-");
+		}
+		System.out.println();
+	}
+}
---
+++ random-quest/src/Status.java
@@ -0,0 +1,46 @@
+public class Status {
+	private String name;
+	private int    hp;
+	private int    maxHp;
+	public Status(String name,int hp){
+		this.name = name;
+		this.hp   = hp;
+		this.maxHp = hp;
+	}
+
+	public void attack(Status target){
+		System.out.printf("%sの攻撃!\n",name);
+		int damage = Main.rand.nextInt(49)+1;
+		target.setHp(target.getHp() - damage);
+		System.out.printf("%sに%dのダメージ!\n",target.getName(),damage);
+	}
+
+	public void heal(){
+		System.out.println("ぶつぶつ言っている・・・");
+		if(maxHp == hp){
+			System.out.println("だが何も起こらなかった");
+		}else{
+			int heal = Main.rand.nextInt(29)+1;
+			if(hp + heal > maxHp) heal = maxHp - hp;
+			this.hp += heal;
+			System.out.printf("%sのHPが%d回復した\n",name,heal);
+		}
+	}
+
+	public void runAway(){
+		System.out.printf("%sは逃げ出した\n",name);
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public void setHp(int hp){
+		this.hp = hp;
+	}
+
+	public int getHp(){
+		return this.hp;
+	}
+
+}
