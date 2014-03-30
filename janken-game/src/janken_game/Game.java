7b74ea3988ccdadc4f7f2c940798b71f8490a56d
junya0215 <junya285@gmail.com>
2014-03-30 13:33:35 +0900

    Fix #1

--- janken-game/src/janken_game/Game.java
+++ janken-game/src/janken_game/Game.java
@@ -5,43 +5,43 @@ import java.util.Scanner;
 
 
 class Game{
-	public static void main(String[] args){
+    public static void main(String[] args){
 
-		Scanner scan = new Scanner(System.in);
-		Random  rand = new Random();
+        Scanner scan = new Scanner(System.in);
+        Random  rand = new Random();
 
-		String[] hands = {"goo","choki","par"};
+        String[] hands = {"goo","choki","par"};
 
-		int player,npc;
+        int player,npc;
 
-		System.out.println("GAME START!");
+        System.out.println("GAME START!");
 
-		while(true){
-			do{
-				System.out.println("Please choose one[goo(0) choki(1) par(2)]");
-				player = scan.nextInt();
-				if(player < 0 || player >= 3)
-					System.out.println("Please input again.");
-			}while(player < 0 || player >= 3);
-			npc    = rand.nextInt(3);
+        while(true){
+            do{
+                System.out.println("Please choose one[goo(0) choki(1) par(2)]");
+                player = scan.nextInt();
+                if(player < 0 || player >= 3)
+                    System.out.println("Please input again.");
+            }while(player < 0 || player >= 3);
+            npc    = rand.nextInt(3);
 
-			//judge
-			System.out.printf("player:%s npc:%s\n",hands[player],hands[npc]);
-			sleep();
+            //judge
+            System.out.printf("player:%s npc:%s\n",hands[player],hands[npc]);
+            sleep();
 
-			System.out.println("--------------------------");
-			Judge.judge(player,npc);
-			if(player != npc){
-				break;
-			}
-		}
+            System.out.println("--------------------------");
+            Judge.judge(player,npc);
+            if(player != npc){
+                break;
+            }
+        }
 
-	}
+    }
 
-	public static void sleep(){
-		try {
-		    Thread.sleep(3000);
-		} catch (InterruptedException e) {
-		}
-	}
+    public static void sleep(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+        }
+    }
 }
--- janken-game/src/janken_game/Judge.java
+++ janken-game/src/janken_game/Judge.java
@@ -2,12 +2,12 @@ package janken_game;
 
 public class Judge {
 
-	public static void judge(int p,int n){
-		switch(p-n){
-			case -1: case  2: System.out.println("You Win"); break;
-			case  1: case -2: System.out.println("You Lose"); break;
-			case  0: System.out.println("drow"); break;
-			default: System.out.println("<<err>> End Game");
-		}
-	}
+    public static void judge(int p,int n){
+        switch(p-n){
+            case -1: case  2: System.out.println("You Win"); break;
+            case  1: case -2: System.out.println("You Lose"); break;
+            case  0: System.out.println("drow"); break;
+            default: System.out.println("<<err>> End Game");
+        }
+    }
 }
